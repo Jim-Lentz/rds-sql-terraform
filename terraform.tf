5b31aaa7ee651de00ce065a8eaf0da9f5f2d26b9
Jim Lentz <jim_lentz@outlook.com>
2024-06-13 15:11:52 -0700

    created main.tf and providers file.

---
+++ terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+  # This version should match what's configured in ops-meta.yaml
+  required_version = "~> 1.8.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.37.0"
+    }
+  }
+  cloud {
+    organization = "TerraformBootCampJimLentzNew"
+    workspaces {
+      name = "rds-sql-terraform"
+    }
+  }
+}
